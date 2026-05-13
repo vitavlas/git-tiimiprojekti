a4bda01e77de5f8a8480b29f1619e532bddf14f8
Jarkko Jalvanti <Juustoleipa207@gmail.com>
2026-05-13 10:51:08 +0300

    Lisätty style.css tiedostoon mukaukksia Hampurilais valikkoa varten. Lisätty script.js toiminto hampurilais valikkoa varten. siirrelty toimintoja index.html tiedostossa ja lisätty hampurilais valikon iconi

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('site-header-scrolled');
     }
 });
+
+
+const HamBurg = document.getElementById("HamIcon");
+const x = document.getElementById("myLinks");
+
+HamBurg.addEventListener("click", () => {
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+    x.style.display = "block";
+  }
+});
