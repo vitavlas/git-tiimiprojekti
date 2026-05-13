422a2542b7b8e49429736c230a089f9d1e0cdd73
Kaapo Kivijärvi <kaapo.kivijarvi@samiedu365.fi>
2026-05-13 09:54:20 +0300

    Luotu alustavat animaatiot etusivulle

--- js/script.js
+++ js/script.js
@@ -2,12 +2,36 @@
 
 
 // navbarin scrollaus
-const navbar =document.querySelector('.site-header');
+const navbar = document.querySelector('.site-header');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >50) {
+    if (window.scrollY > 50) {
         navbar.classList.add('site-header-scrolled');
     } else {
         navbar.classList.remove('site-header-scrolled');
     }
+});
+
+
+// Select all animated elements
+const animatedElements = document.querySelectorAll(
+    ".animate-text, .animate-image, .animate-card"
+);
+
+// Observer for scroll animations
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show-animation");
+        }
+
+    });
+}, {
+    threshold: 0.2
+});
+
+// Observe every element
+animatedElements.forEach((element) => {
+    observer.observe(element);
 });
